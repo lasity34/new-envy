0992e2777dedeabc36bdf6a35a9406302578595c
Bjorn Worrall <85297118+lasity34@users.noreply.github.com>
2024-07-22 20:23:55 +0200

    useuser

--- my-new-envy/src/products/ProductDetails.tsx
+++ my-new-envy/src/products/ProductDetails.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useCart } from "../context/CartContext";
-import { useUser } from "../context/UserContext";
 import styled from "styled-components";
 import { MdCheck } from "react-icons/md";
 import { Product, CartItem } from "../types/types";
